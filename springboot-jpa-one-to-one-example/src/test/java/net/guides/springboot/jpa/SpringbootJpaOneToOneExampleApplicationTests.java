37f3b3331d99c7475fc33a08808570546d4b8bfc
Sourabh Jain <sourabhjain0412@gmail.com>
2022-11-25 16:30:04 +0530

    THis is one to one mapping example

---
+++ springboot-jpa-one-to-one-example/src/test/java/net/guides/springboot/jpa/SpringbootJpaOneToOneExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.springboot.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaOneToOneExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
